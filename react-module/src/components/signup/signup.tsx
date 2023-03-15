3301ec9f5b87080d520d05e36b0fb8c9aa4deee1
taraswww777 <taraswww777@mail.ru>
2023-03-15 19:38:26 +0300

    TaskReactLogin base

--- react-module/src/components/signup/signup.tsx
+++ react-module/src/components/signup/signup.tsx
@@ -1,12 +1,48 @@
+import S from './signup.module.css';
+
 interface SignupProps {
     onSubmit: Function
 }
 
 export const Signup = ({onSubmit}: SignupProps) => {
     return (
-        <div>
+        <div className={S.signup}>
             <form onSubmit={() => onSubmit()}>
-                Signup
+                <div>
+                    <label>
+                        Имя
+                        <input type="text"/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Ник
+                        <input type="text"/>
+                    </label>
+                </div>
+                <div>
+                    <label>Почта
+                        <input type="email"/>
+                    </label>
+                </div>
+                <div>
+                    Пол
+                    <label> M <input type="radio" name="sex" value="M"/></label>
+                    <label> F <input type="radio" name="sex" value="F"/></label>
+                </div>
+                <div>
+                    <label>Пароль
+                        <input type="password" name="password"/>
+                    </label>
+                </div>
+                <div>
+                    <label>Повторить пароль
+                        <input type="password" name="confirmPassword"/>
+                    </label>
+                </div>
+                <div>
+                    <button type="submit">submit</button>
+                </div>
             </form>
         </div>
     )
