e2a1fba969e69347c7b0720fd869f8ab79b8d29e
taraswww777 <taraswww777@mail.ru>
2023-03-15 20:05:43 +0300

    field sex

--- react-module/src/components/signup/signup.tsx
+++ react-module/src/components/signup/signup.tsx
@@ -1,34 +1,66 @@
 import S from './signup.module.css';
+import {useState} from "react";
 
 interface SignupProps {
     onSubmit: Function
 }
 
+enum SEX {
+    M = 'M',
+    F = 'F',
+}
+
+interface FormState {
+    name: string
+    nickname: string
+    email: string,
+    sex: SEX
+}
+
 export const Signup = ({onSubmit}: SignupProps) => {
+    const [inputs, setInputs] = useState<Partial<FormState>>({sex: SEX.M});
+    const onChange = (e: any) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        })
+    }
     return (
         <div className={S.signup}>
-            <form onSubmit={() => onSubmit()}>
+            <p>
+                inputs:<br/>
+                {JSON.stringify(inputs)}
+            </p>
+            <form onSubmit={() => onSubmit()} onChange={onChange}>
                 <div>
                     <label>
                         Имя
-                        <input type="text"/>
+                        <input type="text" name="name" value={inputs?.name}/>
                     </label>
                 </div>
                 <div>
                     <label>
                         Ник
-                        <input type="text"/>
+                        <input type="text" name="nickname" value={inputs?.nickname}/>
                     </label>
                 </div>
                 <div>
                     <label>Почта
-                        <input type="email"/>
+                        <input type="email" name="email" value={inputs?.email}/>
                     </label>
                 </div>
                 <div>
                     Пол
-                    <label> M <input type="radio" name="sex" value="M"/></label>
-                    <label> F <input type="radio" name="sex" value="F"/></label>
+                    <label> M <input
+                        type="radio"
+                        name="sex"
+                        value={SEX.M}
+                    /></label>
+                    <label> F <input
+                        type="radio"
+                        name="sex"
+                        value={SEX.F}
+                    /></label>
                 </div>
                 <div>
                     <label>Пароль
